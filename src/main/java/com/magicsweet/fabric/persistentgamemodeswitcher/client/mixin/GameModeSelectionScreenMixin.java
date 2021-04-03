423afc6d2ae0b8c9209ed3908630a37ba774ea04
MagicSweet <magicsweet920@gmail.com>
2021-04-03 23:29:25 +0300

    v1.0.1
    Fixed bug where mod wasn't working actually changing gamemode :thumbsup:

---
+++ src/main/java/com/magicsweet/fabric/persistentgamemodeswitcher/client/mixin/GameModeSelectionScreenMixin.java
@@ -0,0 +1,15 @@
+package com.magicsweet.fabric.persistentgamemodeswitcher.client.mixin;
+
+import net.minecraft.client.gui.screen.GameModeSelectionScreen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GameModeSelectionScreen.class)
+public class GameModeSelectionScreenMixin {
+	
+	@Redirect(method = "apply(Lnet/minecraft/client/MinecraftClient;Ljava/util/Optional;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasPermissionLevel(I)Z"))
+	private static boolean returnFakePermissionCheck(ClientPlayerEntity clientPlayerEntity, int permissionLevel) {
+	}
+}
